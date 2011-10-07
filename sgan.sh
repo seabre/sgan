a00f4fc401e545ab15cfc76af2303788bca8b717
Sean Brewer <seabre986@gmail.com>
2011-10-07 01:32:20 -0400

    Added netcat stuff.

--- sgan.sh
+++ sgan.sh
@@ -3,19 +3,20 @@
 CREATE_TABLE="CREATE TABLE scan_data (id INTEGER PRIMARY KEY,hash TEXT,location TEXT, status TEXT);";
 
 TEMP_DB=/tmp/sgan.$RANDOM.sqlite
-#TEMP_NETCAT=/tmp/sgan.$RANDOM.netcat
-#TEMP_REPORT=/tmp/sgan.$RANDOM.report.txt
+TEMP_NETCAT=/tmp/sgan.$RANDOM.netcat
 
 sqlite3 $TEMP_DB "$CREATE_TABLE"
 
+echo begin >> $TEMP_NETCAT
 
 (find . -type f -print0 | xargs -0 md5sum)|while read line
 do
  HASH=`echo $line | cut -f1 -d" "`
  LOCATION=`echo $line | cut -f2 -d" "`
- echo $HASH
- echo $LOCATION
  sqlite3 $TEMP_DB  "insert into scan_data (hash,location) values ('$HASH','$LOCATION');"
+ echo $HASH >> $TEMP_NETCAT
 done
 
+echo end >> $TEMP_NETCAT
 
+netcat hash.cymru.com 43 < $TEMP_NETCAT | sed -e '/^\#/d'
