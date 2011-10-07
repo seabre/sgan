43cdf88a21fcaf308c751ed1a2e17a7e1dc6b0c2
Sean Brewer <seabre986@gmail.com>
2011-10-07 00:37:54 -0400

    Fixed issue where hashes and locations weren't being saved in database.

--- sgan.sh
+++ sgan.sh
@@ -2,17 +2,19 @@
 
 CREATE_TABLE="CREATE TABLE scan_data (id INTEGER PRIMARY KEY,hash TEXT,location TEXT, status TEXT);";
 
-TEMP_DB=/tmp/scan.$RANDOM.sqlite
-#TEMP_NETCAT=/tmp/scan.$RANDOM.netcat
-#TEMP_REPORT=/tmp/scan.$RANDOM.report.txt
+TEMP_DB=/tmp/sgan.$RANDOM.sqlite
+#TEMP_NETCAT=/tmp/sgan.$RANDOM.netcat
+#TEMP_REPORT=/tmp/sgan.$RANDOM.report.txt
 
 sqlite3 $TEMP_DB "$CREATE_TABLE"
 
 
 (find . -type f -print0 | xargs -0 md5sum)|while read line
 do
- HASH=$line | cut -f1 -d" "
- LOCATION=$line | cut -f2 -d" "
+ HASH=`echo $line | cut -f1 -d" "`
+ LOCATION=`echo $line | cut -f2 -d" "`
+ echo $HASH
+ echo $LOCATION
  sqlite3 $TEMP_DB  "insert into scan_data (hash,location) values ('$HASH','$LOCATION');"
 done
 
